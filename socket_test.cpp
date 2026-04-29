eda9e4f4f09f57e92c53d943d311683bb276fa29
pratiksapkal333 <pratiksapkal333@gmail.com>
2026-04-29 17:59:53 +0530

    Used While loop to keep server alive forever and handle multiple request

--- socket_test.cpp
+++ socket_test.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <unistd.h>
+#include <unistd.h> // close() is inside this lib
 #include <cstring>
 
 int main() {
@@ -15,29 +15,34 @@ int main() {
 	bind(sock_fd, (struct sockaddr*)&addr, sizeof(addr));
 	listen(sock_fd, 5);
 
-	std::cout << "Waiting for client..." << std::endl;
+	while(true){  // server keeps running forever
+		std::cout << "Waiting for client..." << std::endl;
 
-	int client_fd = accept(sock_fd, NULL, NULL); // waits until client connects
-
-
-	std::cout << "Client connected! FD: " << client_fd << std::endl;
-
-	char buffer[1024] = {0}; // Creat memory to store the incoming data
-
-	read(client_fd, buffer, 1024); //Reads data from client socket and stores it in buffer we just created
-
-	const char* response = 
-		"HTTP/1.1 200 OK\r|n"
-		"Content-Type; test/plain\r\n"
-		"Content-Length: 12\r\n"
-		"\r\n"
-		"Hello World\n";
-
-	write(client_fd, response, strlen(response));
+		int client_fd = accept(sock_fd, NULL, NULL); // waits until client connects
 
 
-	std::cout << "Request received:\n" << buffer << std::endl; // prints the HTTP request
+		std::cout << "Client connected! FD: " << client_fd << std::endl;
 
-	sleep(60);	
+		char buffer[1024] = {0}; // Creat memory to store the incoming data
+
+		read(client_fd, buffer, 1024); //Reads data from client socket and stores it in buffer we just created
+
+		std::cout << "Request received:\n" << buffer << std::endl; // prints the HTTP request
+
+		const char* response = 
+			"HTTP/1.1 200 OK\r|n"
+			"Content-Type; test/plain\r\n"
+			"Content-Length: 12\r\n"
+			"\r\n"
+			"Hello World\n";
+
+		write(client_fd, response, strlen(response));
+		close(client_fd); //closes connection with client , frees file decriptor , prevent resource leak
+
+	}
+
+	
+
+		
 	return 0;
 }
